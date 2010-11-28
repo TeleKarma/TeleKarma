8421ec2d8da55d39d8f348f9e111288947eaa404
MVolk <michael-volk@hotmail.com>
2010-11-28 00:45:15 -0800

    sms.cpp: fixed stub error

--- sms.cpp
+++ sms.cpp
@@ -12,4 +12,5 @@ bool SMS::Send()
 {
 	//XXX Put code to send SMS messages here.
 	cout << endl << "'" << message << "' sent to " << dest << endl;
+	return false;	// added by MV - return value req'd by compiler
 }
